96e14ddc1d62b5b90ac18d366de318db12a0407f
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-05-21 20:40:06 +0100

    Create movies card file with the name of the movies

---
+++ src/Movies.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Movie from './Movie';
+
+const Movies = () => {
+	return (
+		<div className="movies">
+			<Movie name="The Redemption" />
+			<Movie name="Africa's Tech Roots" />
+			<Movie name="The Invisible Man" />
+			<Movie name="The Complicated Project" />
+			<Movie name="Simply Javascript" />
+			<Movie name="The New Web" />
+		</div>
+	)
+}
+
+export default Movies
