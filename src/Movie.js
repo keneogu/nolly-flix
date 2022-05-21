71200051d66f72186f533d6e3eeef0795b0b1c42
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-05-21 20:48:56 +0100

    Add increment function to increment the number of likes per movie

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useState}from 'react'
 
 const Movie = ({name}) => {
+	const [like,setLike] = useState(0);
+
+	const increment = () => {
+		setLike(like + 1);
+	}
 
 	return (
 		<div className="">
 			<h1>{name}</h1>
-			<button>Like!</button>
+			<button onClick={increment}>Like!</button><p>{like}</p>
 		</div>
 	)
 }
